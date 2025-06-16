b3ad44001163c910ff8d75fd81fd6fd500b568ed
raimonvibe <robertjanstefan@gmail.com>
2025-06-16 04:08:46 +0200

    Update page.tsx

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -23,9 +23,16 @@ export default function Clients() {
 
             <div className="posts">
               <article className="common-properties-left">
-                <a href="https://www.collabs.io/mag/raimonvibe/" className="image">
-                  <Image src="/images/pic17.jpg" alt="My company in the online magazine Collabs" width={400} height={300} />
-                </a>
+<a href="https://www.collabs.io/mag/raimonvibe/" className="image">
+  <Image
+    src="/images/pic17.jpg"
+    alt="My company in the online magazine Collabs"
+    width={400}
+    height={300}
+    style={{ width: "100%", height: "auto" }}
+  />
+</a>
+
                 <h3>Interviewed by Collabs</h3>
                 <p>I was interviewed for the online magazine Collabs. I was very surprised when I was asked to write a story about my company. Read my story by clicking on the image.</p>
                 <ul className="actions">
@@ -33,9 +40,19 @@ export default function Clients() {
                 </ul>
               </article>
               <article className="common-properties-right">
-                <a href="https://techbullion.com/is-software-development-a-dead-end-job-after-age-35/" className="image">
-                  <Image src="/images/pic18.jpg" alt="My company in the online magazine TechBullion" width={400} height={300} />
-                </a>
+<a
+  href="https://techbullion.com/is-software-development-a-dead-end-job-after-age-35/"
+  className="image"
+>
+  <Image
+    src="/images/pic18.jpg"
+    alt="My company in the online magazine TechBullion"
+    width={400}
+    height={300}
+    style={{ width: "100%", height: "auto" }}
+  />
+</a>
+
                 <h3>Fintech Magazine TechBullion</h3>
                 <p>I am honored to share that I was recently featured in Fintech Magazine with my insights, published by TechBullion. This is an important milestone in my journey and I am grateful for the opportunity to share my knowledge. Read my story by clicking on the image.</p>
                 <ul className="actions">
