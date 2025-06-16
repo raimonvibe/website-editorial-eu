f610d746971ec9cb97465b4732ba00317ec9b2c2
raimonvibe <robertjanstefan@gmail.com>
2025-06-16 14:50:09 +0200

    Delete src directory

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Header } from './Header'
-export { default as Sidebar } from './Sidebar'
-export { default as SidebarProjects } from './SidebarProjects'
-export { default as Footer } from './Footer'
