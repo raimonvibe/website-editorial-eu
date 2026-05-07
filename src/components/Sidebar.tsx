92c61a8558ed3cfd5efdc690b9a34e65ddd2b61d
raimonvibe <robertjanstefan@gmail.com>
2026-05-07 20:33:41 +0200

    Update Animal Guesses link to Google Play Store

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -105,7 +105,7 @@ export default function Sidebar() {
                 <li><a href="https://www.physics-calculator.raimonvibe.com/" target="_blank">Velocity Calculator</a></li>
                 <li><a href="https://www.physics-flashcards.raimonvibe.com/" target="_blank">Physics Flascards</a></li>
                 <li><a href="https://retro-monitor.raimonvibe.com/" target="_blank">Retro Monitor</a></li>
-                <li><a href="https://animalguesses.raimonvibe.com/" target="_blank">Animal Guesses</a></li>
+                <li><a href="https://play.google.com/store/apps/details?id=com.raimonvibe.animalguesses2" target="_blank">Animal Guesses</a></li>
                 <li><a href="https://www.emf-simulator.raimonvibe.com/" target="_blank">EMF-Simulator</a></li>
                 <li><a href="https://tetris2.0.raimonvibe.com/" target="_blank">Tetris 2.0</a></li>
                 <li><a href="https://periodic-table.raimonvibe.com/" target="_blank">The Periodic Table</a></li>
