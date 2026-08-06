db6ed1d1938b73352fca80336a0f54b619aead30
raimonvibe <robertjanstefan@gmail.com>
2026-08-06 14:17:35 +0200

    Add Ko-fi support widget script to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -185,7 +185,7 @@ export default function RootLayout({
 }}
         />
 
-
+<script type='text/javascript' src='https://storage.ko-fi.com/cdn/widget/Widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('Support me on Ko-fi', '#000000', 'Q0N024KVA1');kofiwidget2.draw();</script> 
  
 
       </body>
